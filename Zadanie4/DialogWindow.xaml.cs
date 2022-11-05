cb2e3627086885b736497346f12fc8183bb7941d
Maciej Czech <maciejek052@gmail.com>
2022-11-05 01:47:24 +0100

    interfejs i wyjebanie komend i mvvm

---
+++ Zadanie4/DialogWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Zadanie4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        bool IsNonCloseButtonClicked; 
+        public DialogWindow()
+        {
+            InitializeComponent();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(InputTextBox.Text))
+            {
+                this.DialogResult = true;
+                Close();
+            }
+        }
+        public int transformationValue
+        {
+            get { return Int32.Parse(InputTextBox.Text); }
+        }
+
+    }
+}
--- Zadanie4/MainWindow.xaml.cs
+++ Zadanie4/MainWindow.xaml.cs
@@ -24,41 +24,21 @@ namespace Zadanie4
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
-    public enum Filter
-    {
-        AVERAGE,
-        MEDIAN,
-        EDGES,
-        HIGHPASS,
-        GAUSSIN
-    }
-    public enum Transformation
-    {
-        ADD,
-        SUBTRACT,
-        MULTIPLY,
-        DIVIDE,
-        BRIGHTNESS,
-        GRAYSCALE1,
-        GRAYSCALE2
-    }
 
     public partial class MainWindow : Window
     {
-        public ICommand ApplayTransformation { get; set; }
-        public ICommand ApplayFilter { get; set; }
-        public ICommand ResetImage { get; set; }
         BitmapImage bitmapImage;
         Bitmap originalBitmap;
         Bitmap currentBitmap;
-        int selectedTransformation;
-        int selectedFilter;
         int transValue;
-        bool imageLoaded;
-        bool transvisible;
-        public string[] Transformations { get; } =new string[]{ "Dodawanie", "Odejmowanie", "Mnożenie", "Dzielenie", "Zmiana jasności", "Skala szarości 1", "Skala szarości 2" };
-        public string[] Filters { get; } = new string[] { "Wygładzający", "Medianowy", "Wykrywania krawędzi", "Górnoprzepustowy wyostrzający", "Rozmycie gaussowskie" };
+        public enum Filter
+        {
+            AVERAGE,
+            MEDIAN,
+            EDGES,
+            HIGHPASS,
+            GAUSSIN
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -76,14 +56,15 @@ namespace Zadanie4
                 bitmapImage2.EndInit();
                 bitmapImage2.Freeze();
                 bitmapImage = bitmapImage2;
+                Img.Source = bitmapImage; 
             }
         }
-        private void Reset(object obj)
+        private void Reset(object sender, RoutedEventArgs e)
         {
             currentBitmap = new Bitmap(originalBitmap);
             LoadBitmap();
         }
-        public void OpenFile()
+        public void openFile(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Title = "Select file";
@@ -93,7 +74,6 @@ namespace Zadanie4
                 originalBitmap=new Bitmap(System.Drawing.Image.FromFile(openFileDialog.FileName));
                 currentBitmap = new Bitmap(originalBitmap);
                 LoadBitmap();
-                imageLoaded = true;
             }
         }
         private byte Filtreaded(int[] mask, List<byte?>values)
@@ -119,132 +99,150 @@ namespace Zadanie4
             byte mediana = (size % 2 != 0) ? (byte)notNull[mid] : (byte)((notNull[mid] + notNull[mid - 1]) / 2);
             return mediana;
         }
-        private void ApplyTrans(object obj)
+        private void ApplyTrans(object obj, RoutedEventArgs e)
         {
-            int pixelID = 0;
-            int x = 0;
-            int y = 0;
-            while (pixelID<currentBitmap.Width*currentBitmap.Height)
+            var mode = ((MenuItem)obj).Tag.ToString();
+            if (currentBitmap != null)
             {
-                var currentPix=currentBitmap.GetPixel(x, y);
-                byte r=currentPix.R;
-                byte g=currentPix.G;
-                byte b=currentPix.B;
-                switch((Transformation)selectedTransformation)
+                if (mode != "grayscale1" && mode != "grayscale2")
                 {
-                    case Transformation.ADD:
-                        r = (byte)((r + transValue) % 256);
-                        g = (byte)((g + transValue) % 256);
-                        b = (byte)((b + transValue) % 256);
-                        break;
-                    case Transformation.SUBTRACT:
-                        r = (byte)((r - transValue) % 256);
-                        g = (byte)((g - transValue) % 256);
-                        b = (byte)((b - transValue) % 256);
-                        break;
-                    case Transformation.MULTIPLY:
-                        r = (byte)((r * transValue) % 256);
-                        g = (byte)((g * transValue) % 256);
-                        b = (byte)((b * transValue) % 256);
-                        break;
-                    case Transformation.DIVIDE:
-                        r = (byte)((r / transValue) % 256);
-                        g = (byte)((g / transValue) % 256);
-                        b = (byte)((b / transValue) % 256);
-                        break;
-                    case Transformation.BRIGHTNESS:
-                        int rB=(int)(r * (transValue / 100.0));
-                        int gB = (int)(g * (transValue / 100.0));
-                        int bB = (int)(b * (transValue / 100.0));
-                        if (rB <0) r = 0;
-                        else if(rB > 255) r = 255;
-                        else r=(byte) rB;
-                        if(gB < 0) g = 0;
-                        else if (gB > 255) g = 255;
-                        else g = (byte)gB;
-                        if (bB < 0) b = 0;
-                        else if (bB > 255) b = 255;
-                        else b = (byte)bB;
-                        break;
-                    case Transformation.GRAYSCALE1:
-                        r = (byte)((r + g + b) / 3);
-                        g = r;
-                        b = r;
-                        break;
-                    case Transformation.GRAYSCALE2:
-                        r = (byte)(r * 0.2126 + g * 0.7152 + b * 0.0722);
-                        g = r;
-                        b = r;
-                        break;
+                    DialogWindow dialog = new DialogWindow();
+                    dialog.Owner = this; 
+                    if (dialog.ShowDialog() == true)
+                        transValue = dialog.transformationValue;
+                    else
+                        transValue = 0;
+                }
+                int pixelID = 0;
+                int x = 0;
+                int y = 0;
+                while (pixelID < currentBitmap.Width * currentBitmap.Height)
+                {
+                    var currentPix = currentBitmap.GetPixel(x, y);
+                    byte r = currentPix.R;
+                    byte g = currentPix.G;
+                    byte b = currentPix.B;
+                    switch (mode)
+                    {
+                        case "add":
+                            r = (byte)((r + transValue) % 256);
+                            g = (byte)((g + transValue) % 256);
+                            b = (byte)((b + transValue) % 256);
+                            break;
+                        case "subtract":
+                            r = (byte)((r - transValue) % 256);
+                            g = (byte)((g - transValue) % 256);
+                            b = (byte)((b - transValue) % 256);
+                            break;
+                        case "multiply":
+                            r = (byte)((r * transValue) % 256);
+                            g = (byte)((g * transValue) % 256);
+                            b = (byte)((b * transValue) % 256);
+                            break;
+                        case "divide":
+                            r = (byte)((r / transValue) % 256);
+                            g = (byte)((g / transValue) % 256);
+                            b = (byte)((b / transValue) % 256);
+                            break;
+                        case "brightness":
+                            int rB = (int)(r * (transValue / 100.0));
+                            int gB = (int)(g * (transValue / 100.0));
+                            int bB = (int)(b * (transValue / 100.0));
+                            if (rB < 0) r = 0;
+                            else if (rB > 255) r = 255;
+                            else r = (byte)rB;
+                            if (gB < 0) g = 0;
+                            else if (gB > 255) g = 255;
+                            else g = (byte)gB;
+                            if (bB < 0) b = 0;
+                            else if (bB > 255) b = 255;
+                            else b = (byte)bB;
+                            break;
+                        case "grayscale1":
+                            r = (byte)((r + g + b) / 3);
+                            g = r;
+                            b = r;
+                            break;
+                        case "grayscale2":
+                            r = (byte)(r * 0.2126 + g * 0.7152 + b * 0.0722);
+                            g = r;
+                            b = r;
+                            break;
 
+                    }
+                    currentBitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(r, g, b));
+                    pixelID++;
+                    x++;
+                    if (currentBitmap.Width <= x)
+                    {
+                        x = 0;
+                        y++;
+                    }
                 }
-                currentBitmap.SetPixel(x,y,System.Drawing.Color.FromArgb(r,g,b));
-                pixelID++;
-                x++;
-                if(currentBitmap.Width<=x)
-                {
-                    x = 0;
-                    y++;
-                }
+                LoadBitmap();
             }
-            LoadBitmap();
         }
-        private void ApplyFilter(object obj)
+        private void ApplyFilter(object obj, RoutedEventArgs e)
         {
-            var bitmap = new Bitmap(currentBitmap.Width, currentBitmap.Height);
-            int pixelID = 0;
-            int x = 0;
-            int y = 0;
-            while(pixelID <currentBitmap.Width*currentBitmap.Height)
+            if (currentBitmap != null)
             {
-                List<byte?> maskR = new List<byte?>();
-                List<byte?> maskG = new List<byte?>();
-                List<byte?> maskB = new List<byte?>();
-                for(int masky=-1;masky<=1;masky++)
-                    for(int maskx=-1;maskx<=1;maskx++)
-                        if(x+maskx>=0 && x+maskx<currentBitmap.Width && y+masky>=0 && y+masky<currentBitmap.Height)
-                        {
-                            var pixel=currentBitmap.GetPixel(x+maskx,y+masky);
-                            maskR.Add(pixel.R);
-                            maskG.Add(pixel.G);
-                            maskB.Add(pixel.B);
-                        }
-                        else
-                        {
-                            maskR.Add(null);
-                            maskG.Add(null);
-                            maskB.Add(null);
-                        }
-                int[] mask = null;
-                switch((Filter)selectedFilter)
+                var mode = ((MenuItem)obj).Tag.ToString();
+                var bitmap = new Bitmap(currentBitmap.Width, currentBitmap.Height);
+                int pixelID = 0;
+                int x = 0;
+                int y = 0;
+                while (pixelID < currentBitmap.Width * currentBitmap.Height)
                 {
-                    case Filter.AVERAGE:
-                        mask = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-                        break;
-                    case Filter.EDGES:
-                        mask = new int[] { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
-                        break;
-                    case Filter.HIGHPASS:
-                        mask = new int[] { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
-                        break;
-                    case Filter.GAUSSIN:
-                        mask = new int[] { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
-                        break;
+                    List<byte?> maskR = new List<byte?>();
+                    List<byte?> maskG = new List<byte?>();
+                    List<byte?> maskB = new List<byte?>();
+                    for (int masky = -1; masky <= 1; masky++)
+                        for (int maskx = -1; maskx <= 1; maskx++)
+                            if (x + maskx >= 0 && x + maskx < currentBitmap.Width && y + masky >= 0 && y + masky < currentBitmap.Height)
+                            {
+                                var pixel = currentBitmap.GetPixel(x + maskx, y + masky);
+                                maskR.Add(pixel.R);
+                                maskG.Add(pixel.G);
+                                maskB.Add(pixel.B);
+                            }
+                            else
+                            {
+                                maskR.Add(null);
+                                maskG.Add(null);
+                                maskB.Add(null);
+                            }
+                    int[] mask = null;
+                    switch (mode)
+                    {
+                        case "average":
+                            mask = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+                            break;
+                        case "edges":
+                            mask = new int[] { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
+                            this.Title = "Sobel - bandyta"; // wyborny easter egg
+                            break;
+                        case "highpass":
+                            mask = new int[] { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+                            break;
+                        case "gaussin":
+                            mask = new int[] { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+                            break;
+                    }
+                    byte r = Filtreaded(mask, maskR);
+                    byte g = Filtreaded(mask, maskG);
+                    byte b = Filtreaded(mask, maskB);
+                    bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(r, g, b));
+                    pixelID++;
+                    x++;
+                    if (currentBitmap.Width <= x)
+                    {
+                        x = 0;
+                        y++;
+                    }
                 }
-                byte r = Filtreaded(mask, maskR);
-                byte g = Filtreaded(mask, maskG);
-                byte b = Filtreaded(mask, maskB);
-                bitmap.SetPixel(x,y, System.Drawing.Color.FromArgb(r,g,b));
-                pixelID++;
-                x++;
-                if (currentBitmap.Width <= x)
-                {
-                    x = 0;
-                    y++;
-                }
+                currentBitmap = bitmap;
+                LoadBitmap();
             }
-            currentBitmap = bitmap;
-            LoadBitmap();
         }
     }
 }
