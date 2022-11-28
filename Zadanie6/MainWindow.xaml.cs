c6830381bc4755cd6a667f1e608403e3023e988c
Maciej Czech <maciejek052@gmail.com>
2022-11-28 03:47:33 +0100

    figury dzialaja

--- Zadanie6/MainWindow.xaml.cs
+++ Zadanie6/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,13 +21,34 @@ namespace Zadanie6
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool bezier = true, shapes = false;
         List<Point> controlPoints = new List<Point>();
         List<Ellipse> Ellipses = new List<Ellipse>();
+
+        List<Point> Points;
+        List<List<Line>> Shapes;
+        List<Line> SelectShape;
+
         public MainWindow()
         {
             InitializeComponent();
+            ShapesBox.Visibility = Visibility.Collapsed;
+            Points = new List<Point>();
+            Shapes = new List<List<Line>>();
+            Shapes.Add(new List<Line>());
+            SelectShape = null;
         }
-
+        private bool IsNumeric(string text)
+        {
+            Regex reg = new Regex("[^0-9]");
+            return reg.IsMatch(text);
+        }
+        private void PrevTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox TB = (TextBox)sender;
+            if (!IsNumeric(TB.Text + e.Text)) e.Handled = false;
+            else { e.Handled = true; TB.Text = "0"; }
+        }
         private void DrawPoints(List<Point> points)
         {
             Path path = MakeCurve(Bezier.GetPoints(points).ToArray(), 0.01);
@@ -98,29 +120,57 @@ namespace Zadanie6
 
         private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Ellipse point = new Ellipse();
-            point.Fill = System.Windows.Media.Brushes.LightGray;
-            point.StrokeThickness = 2;
-            point.Stroke = System.Windows.Media.Brushes.Gray;
-            point.Width = 12;
-            point.Height = 12;
-            point.PreviewMouseMove += ControlPoint_MouseMove;
-            double x = e.GetPosition(this).X;
-            double y = e.GetPosition(this).Y;
-            Canvas.SetLeft(point, x - point.Width / 2);
-            Canvas.SetTop(point, y - point.Width / 2);
-            canvas.Children.Add(point);
-            Canvas.SetZIndex(point, 9999);
-            controlPoints.Add(new Point(x, y));
-            Ellipses.Add(point);
-            indexes.ItemsSource = IndexesList();
+            if (bezier)
+            {
+                Ellipse point = new Ellipse();
+                point.Fill = System.Windows.Media.Brushes.LightGray;
+                point.StrokeThickness = 2;
+                point.Stroke = System.Windows.Media.Brushes.Gray;
+                point.Width = 12;
+                point.Height = 12;
+                point.PreviewMouseMove += ControlPoint_MouseMove;
+                double x = e.GetPosition(this).X;
+                double y = e.GetPosition(this).Y;
+                Canvas.SetLeft(point, x - point.Width / 2);
+                Canvas.SetTop(point, y - point.Width / 2);
+                canvas.Children.Add(point);
+                Canvas.SetZIndex(point, 9999);
+                controlPoints.Add(new Point(x, y));
+                Ellipses.Add(point);
+                indexes.ItemsSource = IndexesList();
+            }
+            else
+            {
+                if (SelectShape != null)
+                    FindShape();
+                LastLine();
+            }
         }
-
+        private void LastLine()
+        {
+            if (Points.Count > 2)
+            {
+                Line line = new Line
+                {
+                    X1 = Points[^1].X,
+                    Y1 = Points[^1].Y,
+                    X2 = Points[0].X,
+                    Y2 = Points[0].Y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 5
+                };
+                Shapes[^1].Add(line);
+                canvas.Children.Add(line);
+                Points.Clear();
+                Shapes.Add(new List<Line>()); 
+            }
+        }
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
             canvas.Children.Clear();
             controlPoints.Clear();
             Ellipses.Clear();
+            Points.Clear();
             indexes.ItemsSource = IndexesList();
         }
         private void Draw_Click(object sender, RoutedEventArgs e)
@@ -199,8 +249,8 @@ namespace Zadanie6
                 point.Fill = System.Windows.Media.Brushes.LightGray;
                 point.StrokeThickness = 2;
                 point.Stroke = System.Windows.Media.Brushes.Gray;
-                point.Width = 16;
-                point.Height = 16;
+                point.Width = 12;
+                point.Height = 12;
                 point.MouseMove += ControlPoint_MouseMove;
                 double x = p.X, y = p.Y;
                 Canvas.SetLeft(point, x - point.Width / 2);
@@ -258,6 +308,253 @@ namespace Zadanie6
                 list.Add(i);
             }
             return list; 
+        }
+
+        private void BezierSelected_Click(object sender, RoutedEventArgs e)
+        {
+            bezier = true;
+            shapes = false;
+            BezierBox.Visibility = Visibility.Visible;
+            ShapesBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (shapes)
+            {
+                if (Shapes[^1].Count == 0)
+                    FindShape();
+                if (SelectShape == null)
+                    AddLine(e.GetPosition(canvas)); 
+            }
+        }
+
+        private void AddPointBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int xVal, yVal;
+            bool failed = false; 
+            if (!Int32.TryParse(X.Text, out xVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość X", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+
+            }
+            if (!Int32.TryParse(Y.Text, out yVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość Y", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            }
+            if (!failed)
+            {
+                AddLine(new Point(xVal, yVal)); 
+            }
+        }
+        private void AddLine(Point p)
+        {
+            Points.Add(p);
+            if (Points.Count > 1)
+            {
+                Line line = new Line
+                {
+                    X1 = Points[^2].X,
+                    Y1 = Points[^2].Y,
+                    X2 = Points[^1].X,
+                    Y2 = Points[^1].Y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 5
+                };
+                Shapes[^1].Add(line);
+                canvas.Children.Add(line); 
+            }
+        }
+        private void FindShape()
+        {
+            foreach (Line Line in canvas.Children.OfType<Line>())
+                if (Line.IsMouseOver)
+                    foreach (var shape in Shapes)
+                        if (shape.Contains(Line))
+                        {
+                            SelectShape = shape;
+                            foreach (var line in SelectShape)
+                                line.Stroke = Brushes.Green;
+                            Line L = SelectShape[0];
+                            X.Text = Convert.ToInt32(L.X1).ToString();
+                            Y.Text = Convert.ToInt32(L.Y1).ToString();
+                            return; 
+                        }
+            SelectShape = null;
+            foreach (Line child in canvas.Children.OfType<Line>()) 
+                child.Stroke = Brushes.Black;
+        }
+
+        private void finishShapeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LastLine();
+        }
+
+        private void moveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int xVal, yVal;
+            bool failed = false;
+            if (!Int32.TryParse(X.Text, out xVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość X", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+
+            }
+            if (!Int32.TryParse(Y.Text, out yVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość Y", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            }
+            if (!failed)
+            {
+                Point Vector = new Point(xVal, yVal);
+                Move(Vector); 
+            }
+        }
+
+        private void rotateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int xVal, yVal;
+            double alpha; 
+            bool failed = false;
+            if (!Int32.TryParse(X.Text, out xVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość X", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+
+            }
+            if (!Int32.TryParse(Y.Text, out yVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość Y", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            }
+            if (!Double.TryParse(rotateTextbox.Text, out alpha))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość alpha", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            } 
+            if (!failed)
+            {
+                alpha = alpha * Math.PI / 180;
+                Point vector = new Point(xVal, yVal);
+                Rotate(vector, alpha); 
+            }
+        }
+
+        private void scaleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int xVal, yVal, alpha;
+            bool failed = false;
+            if (!Int32.TryParse(X.Text, out xVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość X", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+
+            }
+            if (!Int32.TryParse(Y.Text, out yVal))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość Y", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            }
+            if (!Int32.TryParse(rotateTextbox.Text, out alpha))
+            {
+                MessageBox.Show("Podano nieprawidłową wartość alpha", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                failed = true;
+            }
+            if (!failed)
+            {
+                Point vector = new Point(xVal, yVal);
+                Scale(vector, alpha);
+            }
+        }
+
+        private void ShapesSelected_Click(object sender, RoutedEventArgs e)
+        {
+            bezier = false;
+            shapes = true; 
+            BezierBox.Visibility = Visibility.Collapsed;
+            ShapesBox.Visibility = Visibility.Visible;
+        }
+        private void EditShape(object sender, MouseEventArgs e)
+        {
+            if (shapes)
+            {
+                if (SelectShape != null)
+                {
+                    Point Vector = new Point();
+                    foreach (var line in SelectShape)
+                    {
+                        Vector.X += line.X1;
+                        Vector.Y += line.Y1;
+                    }
+                    Vector.X /= SelectShape.Count;
+                    Vector.Y /= SelectShape.Count;
+
+                    if (e.LeftButton == MouseButtonState.Pressed)
+                    {
+                        Vector.X = (e.GetPosition(canvas).X - Vector.X) / 20;
+                        Vector.Y = (e.GetPosition(canvas).Y - Vector.Y) / 20;
+                        Move(Vector);
+                    }
+                    else if (e.RightButton == MouseButtonState.Pressed)
+                    {
+                        double Alpha = (e.GetPosition(canvas).X - Vector.X) / 10000 * Math.PI;
+                        Rotate(Vector, Alpha);
+                    }
+                    else if (e.MiddleButton == MouseButtonState.Pressed)
+                    {
+                        double scale = (10000 + e.GetPosition(canvas).X) / (10000 + Vector.X);
+                        Scale(Vector, scale);
+                    }
+                }
+            }
+        }
+
+        private void Move(Point Vector)
+        {
+            if (SelectShape != null)
+            {
+                foreach (var line in SelectShape)
+                {
+                    line.X1 += Vector.X;
+                    line.X2 += Vector.X;
+                    line.Y1 += Vector.Y;
+                    line.Y2 += Vector.Y;
+                }
+            }
+
+        }
+        private void Rotate(Point Vector, double Alpha)
+        {
+            if (SelectShape != null)
+            {
+                foreach (var line in SelectShape)
+                {
+                    double X1 = line.X1, X2 = line.X2, Y1 = line.Y1, Y2 = line.Y2;
+                    line.X1 = Vector.X + (X1 - Vector.X) * Math.Cos(Alpha) - (Y1 - Vector.Y) * Math.Sin(Alpha);
+                    line.Y1 = Vector.Y + (X1 - Vector.X) * Math.Sin(Alpha) + (Y1 - Vector.Y) * Math.Cos(Alpha);
+
+                    line.X2 = Vector.X + (X2 - Vector.X) * Math.Cos(Alpha) - (Y2 - Vector.Y) * Math.Sin(Alpha);
+                    line.Y2 = Vector.Y + (X2 - Vector.X) * Math.Sin(Alpha) + (Y2 - Vector.Y) * Math.Cos(Alpha);
+                }
+            }
+        }
+        private void Scale(Point Vector, double Scale)
+        {
+            if (SelectShape != null)
+            {
+                foreach (var line in SelectShape)
+                {
+                    double X1 = line.X1, X2 = line.X2, Y1 = line.Y1, Y2 = line.Y2;
+                    line.X1 = Vector.X + (X1 - Vector.X) * Scale;
+                    line.X2 = Vector.X + (X2 - Vector.X) * Scale;
+
+                    line.Y1 = Vector.Y + (Y1 - Vector.Y) * Scale;
+                    line.Y2 = Vector.Y + (Y2 - Vector.Y) * Scale;
+                }
+            }
         }
     }
 }
